eac0c8de3d715d96aa23f6b86e607f5889cf3c09
Karabo Satekge <kb2.satekge@gmail.com>
2025-04-09 19:18:44 +0200

    Views and serializers and filters and admin page

--- StoreBE/Shop/admin.py
+++ StoreBE/Shop/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
+from . import models
 
-# Register your models here.
+@admin.register(models.Product)
+class PoductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'inventory_status']
+    list_display_links = ['title']
+    list_editable = ['price']
+    search_fields = ['title']
+    list_filter = ['price']
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'phone', 'orders']
+    list_editable = ['phone']
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name', 'last_name']
+    list_filter = ['first_name', 'last_name']
+
+    @admin.display(ordering='orders')
+    def orders(self, customer):
+        url = (reverse('admin:Shop_order_changelist')
+               + '?'
+               + urlencode({
+                   'order__id': str(customer.id)
+               }))
+        return format_html('<a href = "{}">{}</a>',url, customer.orders)
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            orders=Count('order')
+        )
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id','placed_at','payment_status', 'customer']
+    list_editable = ['payment_status']
+    search_fields = ['customer']
+    list_filter = ['payment_status', 'placed_at']
+    
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+    search_fields = ['title']
+
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        url = (
+                reverse('admin:Shop_product_changelist')
+               + '?'
+               + urlencode({
+                   'collection__id': str(collection.id)
+               }))
+        return format_html('<a href = "{}">{}</a>',url, collection.products_count)
+    
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+        )
--- StoreBE/Shop/models.py
+++ StoreBE/Shop/models.py
@@ -6,25 +6,44 @@ class Promotion(models.Model):
     start_date = models.DateTimeField(auto_now_add=True)
     end_date = models.DateTimeField()
 
-class Collecction(models.Model):
+class Collection(models.Model):
     title = models.CharField(max_length=200)
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    iventory = models.PositiveIntegerField()
+    inventory = models.PositiveIntegerField()
     last_update = models.DateTimeField(auto_now=True)
-    Collecction = models.ForeignKey(Collecction, on_delete=models.PROTECT)
-    promotions = models.ManyToManyField(Promotion,)
+    Collecction = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering =['title']
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=200)
-    lasst_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=200)
-    birth_date = models.DateTimeField(null=True) 
+    birth_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
+
 
 class Order(models.Model):
     placed_at = models.DateTimeField(auto_now=True)
